f75b749eed493af97f6787524b49cd7b06e129fe
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-12-06 19:45:34 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -5,3 +5,20 @@ const armstrong = num => num === num.toString().split``.reduce((a, b) => a + Mat
 // Kata 2
 
 const bloodMoon = r => (r * r) / 4;
+
+// Kata 3 
+
+// solution 1 
+
+function itinerary(travel) {
+  let itinerary = [];
+  for (let i = 0; i < travel.length; i++) {
+    itinerary.push(travel[i].in);
+    itinerary.push(travel[i].out);
+  }
+  return itinerary.filter((v, i) => v !== itinerary[i + 1]).join("-");
+}
+
+// solution 2 
+
+const itinerary = travel => travel.map(x=>x.in + '-' + x.out).join('-').replace(/(...)-(?=\1)/g, ''); 
